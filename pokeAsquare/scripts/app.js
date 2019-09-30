7feb1086e2239726c3141ae5370a413a9eabae4b
KennyK-TW <b2khakashu@yahoo.com>
2019-09-30 15:47:45 -0700

    Check Final Poke

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,4 +1,5 @@
-console.log('Welcome to Poke-A-Square...');
+console.log('Welcome to Poke-a-Square...');
+// Test comment to show updating in repo
 
 // == User Stories/game logic == //
 // When the user clicks begin, the timer should start and the squares should populate with a random color
@@ -7,49 +8,77 @@ console.log('Welcome to Poke-A-Square...');
 
 // When the round is over, the scores round and timer should be updated for the user to start over with increased difficulty.
 
-// Step One: Add a listener to the Begin and then start game logic
+// Step one Add a listener to the Begin button and then start game logic
 
-$('button').on('click', ()=> {
-    console.log('game start');
-    createSquares(10);
-})
+$("button").on("click", ()=>{
+  console.log("Game Start");
+  createSquares(25);
+});
 
-//----------Step 2-----------------
-// Function needs to start timer and populate Random Squares of random colors
-// grab square container
-// given a number appends to div with a  class #square
+// Step 2
+// Define Function
+// grab the squares container
+// create a loop that runs to the number of squares given
+// for each loop create a square
+// add the square class to it
+// append the square to squares container
 
-const createSquares = (numberOfSquares) => {
-    const $squares = $(".squares");
-    for(let i = 0; i < numberOfSquares; i++){
-        const $square = $('<div class="square"></div>');
-        $square.css("background-color", applyRandomColor());
-        $squares.append($square);
-    }
+const createSquares = numberOfSquares => {
+  const $squares = $(".squares");
+  for(let i=0; i<numberOfSquares; i++){
+    const $square = $('<div class="square"/>');
+    $square.css("background-color", applyRandomColor());
+    $squares.append($square);
+  }
 };
 
-// const createSquares = (numberOfSquares) => {
-//     for(let i = 0; i < numberOfSquares; i++){
-//         $('.squares').append(`<div></div>`);
-        
-//     }
-// };
-// createSquares(5);
-
-//----------Step 3-----------------
-
+// Step three
 // create a function that returns a random color from an array of colors
 
-// const applyRandomColor = () => {
-//     const colors = ["red", "green", "blue"];
-//     const createColors = colors[(Math.floor(Math.random() * colors.length))];
-//     return createColors;
-// }
-// console.log(applyRandomColor());
-
 const applyRandomColor = () => {
-    const colors = ["red", "green", "blue", "rebeccapurple", "black"];
-    const index = Math.floor(Math.random() * colors.length);
-    return colors[index];
-}
-console.log(applyRandomColor());
+  const colors = ["red","green","blue", "purple"];
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+};
+
+// Step 4
+// Add an event listener to square **event delegation** look at the event target and console.log( event target background color)
+
+$(".squares").on("click",".square", (event) => {
+  handlePoke(event);
+});
+
+// create a variable called score and set it to zero
+// in handlePoke() lets grab the color of the current square and save it to a variable
+// Create a new function called checkValidPoke that takes in the square color
+// call that function inside of handlePoke()
+
+let score = 0;
+
+// check if square has been poked
+// if not then we will add poked to the square and change opacity
+// run checkValidPoke with color from the event target
+const handlePoke = event => {
+  if(event.target.classList.contains("poked") === false){
+    const color = $(event.target).css("background-color");
+    $(event.target).addClass("poked").css("opacity",.3);
+    checkValidPoke(color);
+  }
+};
+
+
+// check if color is blue. if so then we will add to score otherwise substract from score
+const checkValidPoke = squareColor => {
+    const colors = squareColor.substring(4,squareColor.length - 1).split(", ");
+    const blueValue = parseInt(colors[2]);
+    console.log(blueValue);
+    //check if blue is equal to 255. if so, add the score, else ---- lower the score
+    if( blueValue === 255 ){
+        score += 1;
+        $('h1').text(`Scoreboard: ${score}`);
+    } else{
+        score -= 1;
+        $('h1').text(`Scoreboard: ${score}`);
+    }
+    
+};
